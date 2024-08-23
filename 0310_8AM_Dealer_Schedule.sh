ec33840a9f3c05c8d7092829b0532e4cdb776613
Bailey.Curtis <155790446+bailey-curtis@users.noreply.github.com>
2024-08-23 12:14:12 +0800

    Add files via upload

---
+++ 0310_8AM_Dealer_Schedule.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+grep -i '08:00:00 AM' 0310_Dealer_schedule.txt | awk -F'\t' '{print $1, $3}' >> Dealers_working_during_losses.txt
+
---
+++ roulette_dealer_finder_by_time.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+grep -i "$2" "$1_Dealer_schedule.txt" | awk -F'\t' '{print $1, $3}'
+
+
